5a90b5bebba469470b7d30d0e1b64d06765d3f70
taschunina <taschunina@yandex.ru>
2020-09-22 13:11:36 +0300

    Настроены схемы сущностей

---
+++ models/article.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true,
+    },
+  text: {
+    type: String,
+    required: true,
+    },
+  date: {
+      type: Date,
+      required: true,
+      default: Date.now
+  },
+  source: {
+    type: String,
+    required: true,
+    },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(link) {
+        return validator.isURL(link);
+      }
+    }
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(link) {
+        return validator.isURL(link);
+      }
+    }
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+});
+
+module.exports = mongoose.model('article', articleSchema);
