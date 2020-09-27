92a501eb4f699b886bf7f330728cd219b63accf1
taschunina <taschunina@yandex.ru>
2020-09-27 17:42:25 +0300

    Сообщения вынесены в отдельный файл

---
+++ middlewares/messages.js
@@ -0,0 +1,13 @@
+const minLengthMessage = 'Длина поля должна быть не менее 2 символов';
+const maxLengthMessage = 'Длина поля должна быть не более 30 символов';
+const requiredMessage = 'Обязательное поле';
+const passwordMessage = 'Пароль должен быть не менее 8 символов';
+const emailMessage = 'Здесь должен быть адрес электронной почты';
+
+module.exports = {
+  minLengthMessage,
+  maxLengthMessage,
+  requiredMessage,
+  passwordMessage,
+  emailMessage,
+};
